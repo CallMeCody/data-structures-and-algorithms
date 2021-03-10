798a3a93255588554895d7447a61ff5596dedafb
Cody Carpenter <cody_carpenter2222@yahoo.com>
2021-03-09 22:35:14 -0800

    code challenge 6

--- javascript/linkedList/__tests__/ll.test.js
+++ javascript/linkedList/__tests__/ll.test.js
@@ -29,4 +29,36 @@ describe('LINKED LIST', () => {
     expect(list.includes('second')).toBeTruthy();
     expect(list.includes('third')).toBeFalsy();
   });
+
+  it('Can successfully add a node to the end of the linked list', () => {
+    let list = new LL();
+    list.append(1);
+    list.append(2);
+    expect(list.head.value).toEqual(1);
+    expect(list.head.next.value).toEqual(2);
+  });
+
+  it('Can successfully insert a node before a node located in the middle of a linked list', () => {
+    let list = new LL();
+    expect(list.insertBefore(2, 22)).toEqual(
+      'Exception: value not found, newValue not inserted'
+    );
+    list.append(1);
+    list.append(3);
+    list.append(3);
+    list.append(2);
+    list.append(3);
+    list.insertBefore(2, 4);
+    expect(list.print()).toEqual([1, 3, 3, 4, 2, 3]);
+  });
+
+  it('Can successfully insert a node after the last node of a linked list', () => {
+    let list = new LL();
+    expect(list.insertBefore(2, 22)).toEqual(
+      'Exception: value not found, newValue not inserted'
+    );
+    list.append(1);
+    list.insertAfter(1, 11);
+    expect(list.head.next.value).toEqual(11);
+  });
 });
